d43bd698022cdc8a1a159d215ab71da85a3116b4
Joao Aroeira <j.aroeira@gmail.com>
2022-10-14 23:54:28 +0000

    add basic CRUD operations to user entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
@@ -11,14 +12,41 @@ export class UsersController {
 
     @Serialize(UserDto)
     @Get()
-    listAllUsers() {
+    findAllUsers() {
         return this.userService.all();
+    }
+
+    @Serialize(UserDto)
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        const userId = parseInt(id);
+        if (Number.isNaN(userId)) return new BadRequestException('id must be a number');
+
+        return this.userService.findOne(userId);
     }
 
     @Serialize(UserDto)
     @Post()
     createUser(@Body() body: CreateUserDto) {
         return this.userService.create(body);
+    }
+
+    @Serialize(UserDto)
+    @Put('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        const userId = parseInt(id);
+        if (Number.isNaN(userId)) return new BadRequestException('id must be a number');
+
+        return this.userService.update(userId, body);
+    }
+
+    @Serialize(UserDto)
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        const userId = parseInt(id);
+        if (Number.isNaN(userId)) return new BadRequestException('id must be a number');
+
+        return this.userService.remove(userId);
     }
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './user.entity';
 import * as bcrypt from 'bcrypt';
 import { BcryptConstants } from '../constants/bcrypt/constants';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -13,10 +14,7 @@ export class UsersService {
 
     async create(userDto: CreateUserDto) {
 
-        //Check in DataBase if email already exists
-        if ((await this.userRepo.findBy({ email: userDto.email })).length > 0) {
-            throw new BadRequestException('Email in use');
-        }
+        await this.throwIfEmailIsNotAvailable(userDto.email);
 
         // Hash password with bcrypt
         const hashedPassword = await bcrypt.hash(userDto.password, BcryptConstants.saltRounds);
@@ -30,8 +28,40 @@ export class UsersService {
         return newUser;
     }
 
+    async findOne(id: number) {
+        const user = await this.userRepo.findOneBy({ id });
+        if (!user) {
+            throw new NotFoundException(`No user found with id ${id}`);
+        }
+
+        return user;
+    }
+
     all(): Promise<User[]> {
         return this.userRepo.find();
     }
 
+    async update(id: number, updateUserDto: UpdateUserDto) {
+        const user = await this.findOne(id);
+
+        await this.throwIfEmailIsNotAvailable(updateUserDto.email, id);
+
+        user.email = updateUserDto.email;
+        const updatedUser = await this.userRepo.save(user);
+        return updatedUser;
+    }
+
+    async remove(id: number) {
+        const user = await this.findOne(id);
+
+        return this.userRepo.remove(user);
+    }
+
+    private async throwIfEmailIsNotAvailable(email: string, id?: number) {
+        //Check in DataBase if email already exists
+        const user = await this.userRepo.findOneBy({ email });
+        if (user && (!id || user.id !== id)) {
+            throw new BadRequestException('Email in use');
+        }
+    }
 }
