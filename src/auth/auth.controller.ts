cb4b658643b693d1c48c6b3cb6b826025470a062
Joao Aroeira <j.aroeira@gmail.com>
2022-10-20 14:45:10 +0000

    add basic signin route and generate jwt token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,23 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 import { AuthUserToReturnDto } from './dtos/auth-user-to-return.dto';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
 
-    constructor() { }
+    constructor(private authService: AuthService) { }
 
-    @Serialize(AuthUserToReturnDto)
-    @UseGuards(AuthGuard('local'))
+    // @Serialize(AuthUserToReturnDto)
+    @UseGuards(LocalAuthGuard)
     @Post('/signin')
     signin(@Request() req) {
+        // If user was successfully validated req obj will have a user property otherwise the route will respond 401
         const { user } = req;
 
-        return user;
+        //generate jwt token
+        return this.authService.signInUser(user);
     }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/users/user.entity';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private usersService: UsersService) { }
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+    ) { }
 
     validateUser(email: string, password: string): Promise<User> {
         return this.usersService.validate(email, password);
+    }
+
+    signInUser(user: User) {
+        const payload = { email: user.email, sub: user.id };
+
+        return {
+            id: user.id,
+            email: user.email,
+            access_token: this.jwtService.sign(payload)
+        };
     }
 
 }
